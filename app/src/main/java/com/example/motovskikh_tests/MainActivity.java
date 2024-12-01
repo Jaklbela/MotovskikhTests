84e09d8ff8206e70f44f37b7cd3a2f3a161800bc
Jaklbela <KsZh11@mail.ru>
2024-12-01 19:51:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/motovskikh_tests/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.motovskikh_tests;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.main_web_view);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://motovskikh.ru/skeleton/");
+    }
+}
