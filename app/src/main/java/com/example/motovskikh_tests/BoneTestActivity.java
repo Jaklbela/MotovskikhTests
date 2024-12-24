acf5e5b39af80775b97e5efac9a773e09668859d
Jaklbela <KsZh11@mail.ru>
2024-12-24 22:54:09 +0300

    Added menu and deeplinks

--- app/src/main/java/com/example/motovskikh_tests/BoneTestActivity.java
+++ app/src/main/java/com/example/motovskikh_tests/BoneTestActivity.java
@@ -1,7 +1,12 @@
 package com.example.motovskikh_tests;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,7 +27,14 @@ public class BoneTestActivity extends AppCompatActivity {
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                startActivity( intent );
+                return true;
+            }
+        });
         webView.loadUrl("https://motovskikh.ru/skeleton/");
 
         webView.setWebChromeClient(new WebChromeClient() {
--- app/src/main/java/com/example/motovskikh_tests/MainActivity.java
+++ app/src/main/java/com/example/motovskikh_tests/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.motovskikh_tests;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,14 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.bone_test);
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, BoneTestActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, BoneTestActivity.class);
+        startActivity(intent);
     }
 }
